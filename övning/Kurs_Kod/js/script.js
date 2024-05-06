77610e913a1506103bcf96918fc82f15e60f884c
LukasKurasinski <146763137+LukasKurasinski@users.noreply.github.com>
2024-05-06 10:42:52 +0200

    fixed script files

---
+++ övning/Kurs_Kod/js/script.js
@@ -0,0 +1,14 @@
+//create object
+
+let car = {
+  wheelsCount: 4,
+  doorCount: 2,
+  regNumber: "abc123",
+  items: ["groceries", "spare wheel", "jack", "blanket"]
+}
+
+//get data with dot -   objektNamn.värde
+console.log(car.wheelsCount); //writes 4 to console
+
+//get data with [] - objektName["värde"]
+console.log(car["regNumber"]);
