9089dde3d232fdc4e41799ad4d01cf9d4d9b16ca
Ulli-m <u.mentori@hotmail.com>
2024-05-07 12:36:14 +0200

    Ulli

---
+++ övning_klasser/Ulli_ Övning klasser/Js/script.js
@@ -0,0 +1,51 @@
+//create class
+
+
+class Plane{
+
+ constructor(nbrSeats,flyesFrom,flyesTo){
+
+    this.numberOfSeats = nbrSeats;
+    this.flyesFrom = flyesFrom;
+    this.flyesTo =flyesTo;
+    this.pilots = ["John", "Maria", "Henry"];
+    
+    }
+
+    addPilot(pilot){
+        this.getPilotesCount.push(pilot);
+    };
+
+    removeLastPilot(){
+        this.pilots.pop();
+    }
+
+    getPilotesCount(){
+
+        return this.pilots.length;
+    };
+ 
+
+};
+
+let bigPlane= new Plane(100,"Dubai", "Mexico City");
+let smallPlane= new Plane(25, "Malmö", "Landskrona");
+
+
+console.log(bigPlane);
+bigPlane.removeLastPilot();
+console.log(bigPlane);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
