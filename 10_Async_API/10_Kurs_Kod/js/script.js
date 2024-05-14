eec31339894d8ebba981ae09292ed58a23a6cef3
LukasKurasinski <146763137+LukasKurasinski@users.noreply.github.com>
2024-05-14 15:16:11 +0200

    fetch ovning

--- 10_Async_API/10_Kurs_Kod/js/script.js
+++ 10_Async_API/10_Kurs_Kod/js/script.js
@@ -28,6 +28,8 @@ function simpleFetch(url){
     })
     .then(data => {
         console.log('Data hämtad:', data);
+        let img = document.getElementById("dog");
+        img.setAttribute("src", data["message"]);
         return data;
     })
     .catch(error => {
@@ -53,11 +55,81 @@ function advancedFetch(url){
 //////////////////
 
 //not synchronised example
-let notSyncResp = fetch("https://catfact.ninja/fact");
-console.log(notSyncResp);
+//let notSyncResp = fetch("https://catfact.ninja/fact");
+//console.log(notSyncResp);
 
 
 //synchronised example
 
-//let apiResponse = simpleFetch("https://catfact.ninja/fact");
-//console.log(apiResponse);
+let apiResponse = simpleFetch("https://catfact.ninja/fact");
+console.log(apiResponse);
+
+
+
+
+let celcToFarh = (c) => {
+  return (c * 9/5) + 32;
+};
+
+console.log(celcToFarh(35));
+
+let farhToCelc = (f) => {
+  return ((f-32) *5)/9;
+}
+console.log(farhToCelc(95));
+
+let hittaMax = (listOfNbrs) => {
+  console.log(listOfNbrs);
+  let temp = listOfNbrs[0];
+  for (let i = 1; i < listOfNbrs.length; i++) {
+    console.log("temp value= "+temp);
+    if(listOfNbrs[i] > temp){
+      console.log("current value "+ listOfNbrs[i]+" > " + temp);
+      temp = listOfNbrs[i];
+    }else{
+      console.log("current value "+ listOfNbrs[i]+" <= " + temp);
+    }
+  }
+  return temp;
+};
+let randomList = [321,432,6,43,21,5,65,325,5,743,2432,632,45,43,766,236,43,6432,6,43];
+hittaMax(randomList);
+
+let createRandomList = (listLength, maxValue) => {
+  let outList = [];
+  for (let i = 0; i < listLength; i++) {
+    let rand = Math.floor(Math.random()*maxValue);
+    outList.push(rand);
+  }
+  return outList;
+}
+console.log(createRandomList(10,50));
+
+
+function add2(a){
+  return a+2;
+}
+function multiply5(a){
+  return a *5;
+}
+
+let result = multiply5(add2(10));
+ 
+console.log(result);
+
+let text = "this is a text".toUpperCase();
+let textReplace = text.replace("THIS", "that");
+let textReverse = textReplace.concat("new text added");
+console.log(textReverse);
+
+let text2 = "this is another text".toUpperCase().replace("THIS", "that").concat("new text added");
+console.log(text2);
+
+
+
+function showData(){
+  let url = document.getElementById("url").value;
+  let apiResponse = simpleFetch(url);
+
+  //console.log(document.getElementById("text").value);
+}
